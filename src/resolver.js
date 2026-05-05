184e7dc5df20d741c748d69767c094da6ced0ae9
Neykoor <cafecafirexos@gmail.com>
2026-05-05 13:37:25 -0700

    Update resolver.js

--- src/resolver.js
+++ src/resolver.js
@@ -23,7 +23,6 @@ export class LidResolver {
   #cache;
   #sock;
   #store;
-  #db;
   #reverseIndex = new Map();
   #handler;
   #msgHandler;
@@ -42,7 +41,6 @@ export class LidResolver {
   constructor(sock, options = {}) {
     this.#sock = sock;
     this.#store = options.store || null;
-    this.#db = options.db || null;
     this.#cache = new LidCache(options.cache || {});
     this.#maxIndexSize = Math.max(1000, options.maxIndexSize || 50000);
     this.#indexTtl = options.indexTtlMs ?? 21600000;
@@ -193,13 +191,6 @@ export class LidResolver {
       }
     };
 
-    if (this.#db) {
-      try {
-        const rows = this.#db.load();
-        if (rows.length) this.precargarCache(rows.map((r) => [r.lid, r.jid]));
-      } catch {}
-    }
-
     this.sincronizarDesdeStore();
     this.#suscribirAEventos();
   }
@@ -218,7 +209,6 @@ export class LidResolver {
   }
 
   #guardarEnSignalRepository(pares) {
-    try { this.#db?.save(pares); } catch {}
     this.#sock.signalRepository?.lidMapping?.storeLIDPNMappings?.(pares)?.catch(() => {});
   }
 
@@ -449,7 +439,6 @@ export class LidResolver {
     this.#purgeInterval = null;
     this.#cache.destroy();
     this.#reverseIndex.clear();
-    try { this.#db?.close(); } catch {}
     this.#sock.ev.off("contacts.upsert", this.#handler);
     this.#sock.ev.off("contacts.update", this.#handler);
     this.#sock.ev.off("messages.upsert", this.#msgHandler);
@@ -462,5 +451,5 @@ export class LidResolver {
     this.#sock.ev.off("groups.update", this.#groupsUpsertHandler);
     this.#joinCallbacks = [];
   }
-          }
+      }
       
